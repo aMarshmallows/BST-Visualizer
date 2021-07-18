e7e30440b5a6b60b6b0f8f9a5b048c4d33fb99c4
aMarshmallows <amasha667@gmail.com>
2021-07-18 14:27:28 -0400

    edges place somewhat correctly when inserting

--- index.js
+++ index.js
@@ -17,24 +17,29 @@ class Node {
     }
 }
 
+function getRadius(cy) {
+    let radius = 0;
+    let height = (parseInt(cy) - 100) / 80;
+    if (height > 4) {
+        radius = 12.5;
+    }
+    else {
+        radius = (30 - height * 3);
+    }
+    return radius;
+}
+
 // creates the circle and centers text within for a node
 function createNodeInDOM(data, cx, cy) {
+    let height, radius = 0;
     const nodeCircle = document.createElementNS(svgns, "circle");
     svg.appendChild(nodeCircle);
     nodeCircle.setAttribute("cx", cx);
     nodeCircle.setAttribute("cy", cy);
 
     // decrease circle radius as height increases
-    let height = (parseInt(cy) - 100) / 80;
-    let radius = 25;
-    if (height > 4) {
-        radius = 12.5.toString();
-    }
-    else {
-        radius = (30 - height * 3).toString();
-    }
 
-    nodeCircle.setAttribute("r", radius);
+    nodeCircle.setAttribute("r", getRadius(cy).toString());
     nodeCircle.setAttribute("stroke", "green");
     nodeCircle.setAttribute("stroke-width", "4");
     nodeCircle.setAttribute("fill", "yellow");
@@ -50,7 +55,55 @@ function createNodeInDOM(data, cx, cy) {
 }
 
 //
-//function createEdgeInDOM(parentCx, parentCy,)
+function createEdgeInDOM(parentNode, currNode) {
+    currNodeCxInt = parseInt(currNode.cx);
+    currNodeCyInt = parseInt(currNode.cy);
+    parentNodeCxInt = parseInt(parentNode.cx);
+    parentNodeCyInt = parseInt(parentNode.cy);
+    let height, currRadius, parentRadius, angle, x1, y1, x2, y2 = 0;
+
+    const edge = document.createElementNS(svgns, "line");
+    svg.appendChild(edge);
+    edge.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:3");
+
+    // gets radius of currNode
+    currRadius = getRadius(currNode.cy);
+
+    // gets radius of parentNode
+    parentRadius = getRadius(parentNode.cy);
+
+
+    // calculate xdifference and ydifference
+    cxDif = Math.abs(currNodeCxInt - parentNodeCxInt);
+    cyDif = currNodeCyInt - parentNodeCyInt;
+    // calculates length of line
+    Math.hypot(cyDif, cxDif);
+    // calculates angle from currNode to parentNode from x axis (no negatives)
+    angle = Math.atan(cyDif / cxDif) * (180 / Math.PI);
+    console.log(angle);
+    // the percent of 625 that should be change in cy and in cx
+    cyProportion = angle / 90;
+    cxProportion = 1 - cyProportion;
+    // calculates the difference in cy to achieve hypot of 25 (25^2 is 625)
+    y2 = currNodeCyInt - Math.sqrt(currRadius * currRadius * cyProportion);
+    y1 = parentNodeCyInt + Math.sqrt(parentRadius * parentRadius * cyProportion);
+
+    // 
+    if (currNodeCxInt < parentNodeCxInt) {
+        x1 = parentNodeCxInt - Math.sqrt(parentRadius * parentRadius * cxProportion)
+        x2 = currNodeCxInt + Math.sqrt(parentRadius * parentRadius * cxProportion);
+    }
+    else {
+        x1 = parentNodeCxInt + Math.sqrt(parentRadius * parentRadius * cxProportion)
+        x2 = currNodeCxInt - Math.sqrt(currRadius * currRadius * cxProportion);
+    }
+
+    edge.setAttribute("x1", x1.toString());
+    edge.setAttribute("y1", y1.toString());
+    edge.setAttribute("y2", y2.toString());
+    edge.setAttribute("x2", x2.toString());
+
+}
 
 // Binary Search tree class
 class BinarySearchTree {
@@ -81,19 +134,15 @@ class BinarySearchTree {
     //calcCxCy(parentNode)
 
     insertNode(node, newNode, height) {
-        console.log("prev node val: " + node.data);
-        console.log("curr node data: " + newNode.data);
-        console.log(typeof node.data);
-        console.log(typeof newNode.data);
         if (newNode.data < node.data) {
             // if left is null insert node here
             if (node.left == null) {
-                console.log("node going left");
                 node.left = newNode;
                 newNode.cx = (parseInt(node.cx) - 400 * (Math.pow(0.5, height))).toString();
                 newNode.cy = (parseInt(node.cy) + 80).toString();
-                console.log(newNode.cx);
+                createEdgeInDOM(node, newNode);
                 createNodeInDOM(newNode.data, newNode.cx, newNode.cy);
+
             }
 
             else {
@@ -111,8 +160,9 @@ class BinarySearchTree {
                 node.right = newNode;
                 newNode.cx = (parseInt(node.cx) + 400 * (Math.pow(0.5, height))).toString();
                 newNode.cy = (parseInt(node.cy) + 80).toString();
-                console.log(newNode.cx);
+                createEdgeInDOM(node, newNode);
                 createNodeInDOM(newNode.data, newNode.cx, newNode.cy);
+
             }
             else {
                 // find null by recursion
@@ -155,3 +205,5 @@ insertButton.onclick = function insertVal() {
 
 //<circle cx="50" cy="50" r="25" stroke="green" stroke-width="4" fill="yellow" />
 //<text x="50" y="50" dominant-baseline="middle" text-anchor="middle">TEXT</text>
+//<line x1="400" y1="100" x2="200" y2="180" style="stroke:rgb(0, 0, 0);stroke-width:2" />
+// <line x1="400" y1="100" x2="600" y2="180" style="stroke:rgb(255,0,0);stroke-width:2" />
